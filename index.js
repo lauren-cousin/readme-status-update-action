0dcb75efe14f78ddf856dbc66b04fe6f9ca897a9
Lauren Cousin <lauren.cousin@gmail.com>
2020-07-22 20:03:43 -0600

    Some tweaks

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ try {
   // `your-status` input defined in action metadata file
   const yourStatus = core.getInput('your-status');
   console.log(`Current Status: ${yourStatus}`);
+  core.setOutput("yourStatus", yourStatus);
   const time = (new Date()).toTimeString();
+  console.log(`Current Time: ${time}`);
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
